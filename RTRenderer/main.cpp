e24aeab6b3fbe7db3f184116274cd52650714217
VascoUP <vasco_mag_pereira@hotmail.com>
2018-10-06 15:32:42 +0200

    First shaded pyramid rotating

--- RTRenderer/main.cpp
+++ RTRenderer/main.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
 // Window dimension
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265359f / 180.0f;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, EBO, shader, uniformModel;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.0005f;
+
+float currRot = 0.0f;
+float deltaRot = 0.05f;
 
 // Vertex Shader code
 static const char* vShader = "										\n\
@@ -15,18 +29,25 @@ static const char* vShader = "										\n\
 																	\n\
 layout (location = 0) in vec3 pos;									\n\
 																	\n\
+out vec4 vCol;														\n\
+																	\n\
+uniform mat4 model;													\n\
+																	\n\
 void main() {														\n\
-	gl_Position = vec4(0.4 * pos, 1.0);	\n\
+	gl_Position = model * vec4(pos.xyz, 1.0);						\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);						\n\
 }";
 
 // Fragment Shader
 static const char* fShader = "										\n\
 #version 330														\n\
 																	\n\
+in vec4 vCol;														\n\
+																	\n\
 out	vec4 colour;													\n\
 																	\n\
 void main() {														\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);								\n\
+	colour = vCol;													\n\
 }";
 
 // Err Vertex Shader
@@ -50,14 +71,25 @@ void main() {														\n\
 }";
 
 void CreateTriangle() {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 1.0f,
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -67,11 +99,12 @@ void CreateTriangle() {
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
 }
 
-int AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+bool AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
 {
 	GLuint theShader = glCreateShader(shaderType);
 
@@ -91,28 +124,28 @@ int AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
 	if (!result)
 	{
 		glGetShaderInfoLog(theShader, 1024, NULL, eLog);
-		fprintf(stderr, "Error compiling the %d shader: '%s'\n", shaderType, eLog);
-		return 0;
+		printf("Error compiling the %d shader: '%s'\n", shaderType, eLog);
+		return false;
 	}
 
 	glAttachShader(theProgram, theShader);
 
-	return 1;
+	return true;
 }
 
-int CompileShaders() {
+bool CompileShaders() {
 	shader = glCreateProgram();
 
 	if (!shader) {
 		printf("Error creating shader program");
-		return 1;
+		return false;
 	}
 
 	if (!AddShader(shader, vShader, GL_VERTEX_SHADER)) {
-		return 0;
+		return false;
 	}
 	if (!AddShader(shader, fShader, GL_FRAGMENT_SHADER)) {
-		return 0;
+		return false;
 	}
 
 	GLint result = 0;
@@ -129,7 +162,7 @@ int CompileShaders() {
 		AddShader(shader, errFShader, GL_FRAGMENT_SHADER);
 		glLinkProgram(shader);
 		glValidateProgram(shader);
-		return 0;
+		return false;
 	}
 
 	glValidateProgram(shader);
@@ -143,34 +176,37 @@ int CompileShaders() {
 		AddShader(shader, errFShader, GL_FRAGMENT_SHADER);
 		glLinkProgram(shader);
 		glValidateProgram(shader);
-		return 0;
+		return false;
 	}
 
+	uniformModel = glGetUniformLocation(shader, "model");
 
-	return 1;
+	return true;
 }
 
-int ErrShader() {
+bool ErrShader() {
 	shader = glCreateProgram();
 
 	if (!shader) {
 		printf("Error creating shader program");
-		return 0;
+		return false;
 	}
 
 	AddShader(shader, errVShader, GL_VERTEX_SHADER);
 	AddShader(shader, errFShader, GL_FRAGMENT_SHADER);
 	glLinkProgram(shader);
 	glValidateProgram(shader);
+
+	return true;
 }
 
-int GetShaders() {
+bool GetShaders() {
 	if (!CompileShaders()) {
 		if (!ErrShader()) {
-			return 0;
+			return false;
 		}
 	}
-	return 1;
+	return true;
 }
 
 int main() {
@@ -214,6 +250,8 @@ int main() {
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup viewport
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -222,21 +260,42 @@ int main() {
 		glfwDestroyWindow(mainWindow);
 		glfwTerminate();
 	}
-
+	
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow)) {
-
 		// Get + Handle user input events
 		glfwPollEvents();
 
+		if (direction) {
+			triOffset += triIncrement;
+		}
+		else {
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxOffset) {
+			direction = !direction;
+		}
+
+		currRot += deltaRot;
+
 		// Clear Window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
+		
+		glm::mat4 model;
+		//model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+		model = glm::rotate(model, currRot * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 1.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 
 		glUseProgram(0);
